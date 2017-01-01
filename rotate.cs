4d2677e700505042af791d82e1e7459c181f2c09
SunrayIsbro <lebed2@i.ua>
2017-01-02 01:12:35 +0300

    Add files via upload

---
+++ rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    public int speedrt;
+
+	void Start ()
+    {
+
+	}
+
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, 1 * speedrt * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -1 * speedrt * Time.deltaTime);
+        }
+    }
+}
